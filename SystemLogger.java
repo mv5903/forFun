c60616227ba7102c9da40d907821829d0b169aae
Matthew Vandenberg <mv5903@gmail.com>
2020-08-14 17:49:48 -0400

    Update SystemLogger.java

--- SystemLogger.java
+++ SystemLogger.java
@@ -9,14 +9,19 @@ import java.lang.management.OperatingSystemMXBean;
 import java.util.Date;
 import java.util.Map;
 import java.util.Scanner;
-
+/**
+ * Logs all system information to one text file of the users choosing, based on a particular path.
+ * @author matt
+ *
+ */
 public class SystemLogger {
-
+	//Throw IOException because files could end up not working
 	public static void main(String[] args) throws IOException {
 		Scanner kbd = new Scanner(System.in);
-		File systemLogger;
+		File systemLogFile;
 		FileWriter writer;
 		String path;
+		//Helps determine when the file is valid or not
 		boolean exceptionOccured = false;
 		while (true) {
 			System.out.println(
@@ -24,8 +29,8 @@ public class SystemLogger {
 			path = kbd.next();
 			try {
 				exceptionOccured = false;
-				systemLogger = new File(path + "\\systemLogger.txt");
-				writer = new FileWriter(systemLogger);
+				systemLogFile = new File(path + "\\systemLogger.txt");
+				writer = new FileWriter(systemLogFile);
 			} catch (FileNotFoundException f) {
 				exceptionOccured = true;
 				System.out.println(
@@ -36,26 +41,33 @@ public class SystemLogger {
 				break;
 			}
 		}
-
-		systemLogger = new File(path + "\\systemLogger.txt");
-		if (systemLogger.exists()) {
-			systemLogger.delete();
+		//Create the file:
+		systemLogFile = new File(path + "\\systemLogger.txt");
+		if (systemLogFile.exists()) {
+			systemLogFile.delete();
 		}
-		writer = new FileWriter(systemLogger);
+		//Create the FileWriter object which will write to the text file given via the file systemLogger:
+		writer = new FileWriter(systemLogFile);
+		//System properties are stored in a Map, so get them accordingly:
 		Map<String, String> systemProperties = System.getenv();
 		Date currentDate = new Date();
 		writer.write("File last updated on " + currentDate.toString() + "\n\n\nSystem Information: \n\n");
+		//Write each key and value to the text file
 		for (Map.Entry<String, String> e : systemProperties.entrySet()) {
 			writer.write(e.getKey() + ": " + e.getValue() + "\n");
 		}
 		writer.write("\n\nRuntime Information: \n\n");
 		Runtime.getRuntime();
+		//Runtime Properties are not accessible via a map, so store them each in an array accordingly:
 		String[] runtimeInfo = {"Available processors (cores): " + Runtime.getRuntime().availableProcessors(), "Free memory: " + Runtime.getRuntime().freeMemory() + " B (" + String.format("%.3f", toGB(Runtime.getRuntime().freeMemory())) + " GB)", "Maximum Memory: " + Runtime.getRuntime().maxMemory() + " B (" + String.format("%.3f", toGB(Runtime.getRuntime().maxMemory())) + " GB)", "Total Memory: " + Runtime.getRuntime().totalMemory() + " B (" + String.format("%.3f", toGB(Runtime.getRuntime().totalMemory())) + " GB)", "Version: " + Runtime.version()};
+		//Write each element of the runtimeInfo String array to the systemLogFile:
 		for (String s: runtimeInfo) {
 			writer.write(s + "\n");
 		}
 		writer.write("\n\nFile Roots: \n\n");
+		//File Roots are slightly easier, stored nicely in an array:
 		File[] roots = File.listRoots();
+		//For each is necessary in order to access each file root (C drive, D drive, etc.):
 		for (File root: roots) {
 			writer.write("File system root: " + root.getAbsolutePath() + "\n");
 		    writer.write(String.format("Total space: %d B (%.3f GB)\n", root.getTotalSpace(), toGB(root.getTotalSpace())));
@@ -63,16 +75,22 @@ public class SystemLogger {
 		    writer.write(String.format("Used space: %d B, (%.3f GB)\n", root.getTotalSpace()-root.getFreeSpace(), toGB(root.getTotalSpace()-root.getFreeSpace())) + "\n");
 		}
 		writer.write("\nOS Info:\n\n");
+		//OperatingSystemMXBean is a package responsible for certain cpu and system information:
 		OperatingSystemMXBean os = ManagementFactory.getOperatingSystemMXBean();
 		String[] osInfo = {"Name of Operating System: " + os.getName(), "Architecture Type: " + os.getArch(), "Available Processors: " + os.getAvailableProcessors(), "System Load Average: " + os.getSystemLoadAverage()};
 		for (String s: osInfo) {
 			writer.write(s + "\n");
 		}
 		System.out.println("Done!");
+		//Need to close the Scanner and FileWriter when done:
 		kbd.close();
 		writer.close();
 	}
-	
+	/**
+	 * Converts a long number in bytes to a floating point value in Gigabytes (GB)
+	 * @param bytes Number in bytes
+	 * @return bytes converted to Gigabytes (GB)
+	 */
 	public static double toGB(long bytes) {
 		return bytes / 1024.0 / 1024.0 / 1024.0;
 	}
